57bcbd04f9a6d71bb6bfcffd34e8c293a566f01d
shurikek3990 <shurik.shelest@gmail.com>
2025-04-12 15:16:11 +0200

    First App Ver

---
+++ App/ShieldShed/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShieldShed
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App/ShieldShed/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShieldShed
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
